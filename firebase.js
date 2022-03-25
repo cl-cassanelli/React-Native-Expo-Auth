48b5e0516945a01ac61aa8fbf14950c081fcaace
cl_cassanelli <erbapippo155@gmail.com>
2022-03-25 09:25:57 +0100

    Upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
